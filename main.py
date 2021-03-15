aeae7d80bc4499c025197c10f33d4252fe4e7aed
kenjipy <80217235+kenjipy@users.noreply.github.com>
2021-03-15 10:39:23 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import random
+
+chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456!çàù"
+# This is for your character i added a coppel basic ones you can add more if you want. #
+
+while 1:
+    password_len = int(input("what length would you like your password to be : "))
+    password_count = int(input("how many password would you like : "))
+    for x in range(0,password_count):
+        password = ""
+        for x in range(0,password_len):
+            password_char = random.choice(chars)
+            password      = password + password_char
+        print("here is your password = ", password)
